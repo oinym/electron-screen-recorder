1bb85674875c65eb50351862c472482837b96c47
Ofir Dayan <Utytyih@Ofirs-MacBook-Pro.local>
2024-10-18 19:05:08 +0300

    continues

--- src/main.js
+++ src/main.js
@@ -49,6 +49,22 @@ app.on('activate', () => {
         session.defaultSession.setPermissionRequestHandler((webContents,permission,callback)=>{
             return callback(false)
         })
+        session.defaultSession.setDisplayMediaRequestHandler(
+          (request, callback) => {
+            desktopCapturer
+              .getSources({ types: ["screen"] })
+              .then((sources) => {
+                // Grant access to the first screen found.
+                callback({ video: sources[0], audio: "loopback" });
+              });
+            // If true, use the system picker if available.
+            // Note: this is currently experimental. If the system picker
+            // is available, it will be used and the media request handler
+            // will not be invoked.
+          },
+          { useSystemPicker: true }
+        );
+
     }
 });
 
--- src/renderer.js
+++ src/renderer.js
@@ -1,107 +1,115 @@
-const {ipcRenderer}=require('electron')
-const {writeFile}=require('fs')
+const { ipcRenderer } = require("electron");
+const { writeFile } = require("fs");
 let mediaRecorder;
 let recordedChunks = [];
 
 // Buttons
-const videoElement = document.querySelector('video');
+const videoElement = document.querySelector("video");
 
-const recBtn = document.getElementById('recBtn');
-recBtn.onclick = e => {
-    console.log('start recording button pressed')
-    startRecording();
-  recBtn.innerText = 'Recording';
+const recBtn = document.getElementById("recBtn");
+recBtn.onclick = (e) => {
+  console.log("start recording button pressed");
+  startRecording();
+  recBtn.innerText = "Recording";
 };
 
-const stopBtn = document.getElementById('stopBtn');
+const stopBtn = document.getElementById("stopBtn");
 
-stopBtn.onclick = e => {
+stopBtn.onclick = (e) => {
   mediaRecorder.stop();
-  recBtn.innerText = 'Start';
+  recBtn.innerText = "Start";
 };
 
 // const videoSelectBtn = document.getElementById('videoSelectBtn');
 // videoSelectBtn.onclick = getVideoSources;
 
-const selectMenu = document.getElementById('selectMenu')
+const selectMenu = document.getElementById("selectMenu");
 
 async function getVideoSources() {
-    const inputSources = await ipcRenderer.invoke('getSources')
-  
-    inputSources.forEach(source => {
-      const element = document.createElement("li")
-      const preview=document.createElement('img')
-      const text=document.createElement('span')
-        preview.src=source.thumbnail.toPNG();
-      
-      text.innerHTML =  source.name
-      element.appendChild(preview)
-      element.appendChild(text)
-      element.value = source.id
-      selectMenu.appendChild(element)
-    });
-  }
+  const inputSources = await ipcRenderer.invoke("getSources");
 
+  inputSources.forEach((source) => {
+    console.log("Sources: ", { inputSources });
+    const element = document.createElement("li");
+    const preview = document.createElement("img");
+    const text = document.createElement("span");
+    preview.src = source.thumbnail.toDataURL();
 
-  async function startRecording() {
-    getVideoSources();
-    if(selectMenu.options[selectMenu.selectedIndex]){
-        const screenId = selectMenu.options[selectMenu.selectedIndex].value
+    text.innerHTML = source.name;
+    element.appendChild(preview);
+    element.appendChild(text);
+    element.value = source.id;
+    element.onclick =  record(source.id);
     
-        // AUDIO WONT WORK ON MACOS
-        const IS_MACOS = await ipcRenderer.invoke("getOperatingSystem") === 'darwin'
-        console.log(await ipcRenderer.invoke('getOperatingSystem'))
-        const audio = !IS_MACOS ? {
-          mandatory: {
-            chromeMediaSource: 'desktop'
-          }
-        } : false
-      
-        const constraints = {
-          audio,
-          video: {
-            mandatory: {
-              chromeMediaSource: 'desktop',
-              chromeMediaSourceId: screenId
-            }
-          }
-        };
-      
-        // Create a Stream
-        const stream = await navigator.mediaDevices
-          .getUserMedia(constraints);
-      
-        // Preview the source in a video element
-        videoElement.srcObject = stream;
-        await videoElement.play();
-      
-        mediaRecorder = new MediaRecorder(stream, { mimeType: 'video/webm; codecs=vp9' });
-        mediaRecorder.ondataavailable = onDataAvailable;
-        mediaRecorder.onstop = stopRecording;
-        mediaRecorder.start();
-    }
-    
-  }
-
-function onDataAvailable(e) {
-    recordedChunks.push(e.data);
+    selectMenu.appendChild(element);
+  });
+  if (inputSources) selectMenu.style.visibility = "visible";
 }
 
+async function record(id) {
+  // console.log('chosen screen', id)
+  selectMenu.style.visibility = "hidden";
+  const screenId = id;
+  recBtn.setAttribute("disabled", true);
+  // AUDIO WONT WORK ON MACOS
+  const IS_MACOS =
+    (await ipcRenderer.invoke("getOperatingSystem")) === "darwin";
+  console.log(await ipcRenderer.invoke("getOperatingSystem"));
+  const audio = !IS_MACOS
+    ? {
+        mandatory: {
+          chromeMediaSource: "desktop",
+        },
+      }
+    : false;
+
+  const constraints = {
+    audio,
+    video: {
+      mandatory: {
+        chromeMediaSource: "desktop",
+        chromeMediaSourceId: screenId,
+      },
+    },
+  };
+
+  // Create a Stream
+  const stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+  // Preview the source in a video element
+  videoElement.srcObject = stream;
+  await videoElement.play();
+
+  mediaRecorder = new MediaRecorder(stream, {
+    mimeType: "video/webm; codecs=vp9",
+  });
+  mediaRecorder.ondataavailable = onDataAvailable;
+  mediaRecorder.onstop = stopRecording;
+  mediaRecorder.start();
+}
+
+async function startRecording() {
+  getVideoSources();
+}
+
+function onDataAvailable(e) {
+  recordedChunks.push(e.data);
+}
 
 async function stopRecording() {
-    videoElement.srcObject = null
+  videoElement.srcObject = null;
 
-    const blob = new Blob(recordedChunks, {
-      type: 'video/webm; codecs=vp9'
-    });
-  
-    const buffer = Buffer.from(await blob.arrayBuffer());
-    recordedChunks = []
+  const blob = new Blob(recordedChunks, {
+    type: "video/webm; codecs=vp9",
+  });
 
-    const { canceled, filePath } =  await ipcRenderer.invoke('showSaveDialog')
-    if(canceled) return
-  
-    if (filePath) {
-      writeFile(filePath, buffer, () => console.log('video saved successfully!'));
-    }
+  const buffer = Buffer.from(await blob.arrayBuffer());
+  recordedChunks = [];
+
+  const { canceled, filePath } = await ipcRenderer.invoke("showSaveDialog");
+  if (canceled) return;
+
+  if (filePath) {
+    writeFile(filePath, buffer, () => console.log("video saved successfully!"));
   }
+}
